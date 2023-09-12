921bed2d3e5141dafddbf3246c6fe239936a528c
Minou90 <amina.green.smart@gmail.com>
2023-09-12 16:05:11 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Entry point
- * Return : Always 0 
+ * Return: Always 0
  */
 int main(void)
 {
@@ -15,5 +15,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
