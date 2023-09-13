790f3589cfba7fe86c58b8723f6e07eeb31f90d0
Minou90 <amina.green.smart@gmail.com>
2023-09-12 19:29:23 -0700

    24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,11 +5,13 @@
  * jack_bauer - prints each minute of the day
  *
  * Return:returns 0
- */ 
+ */
+
 void jack_bauer(void)
 {
 
 	int min, hour;
+
 	for (hour = 0; hour <= 23; hour++)
 	{
 	for (min = 0; min <= 59; min++)
