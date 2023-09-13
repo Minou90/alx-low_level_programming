e101faa5235a71eddce2f31d11799262ceaae2d0
Minou90 <amina.green.smart@gmail.com>
2023-09-12 18:54:25 -0700

    abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -7,11 +7,10 @@
  */
 int _abs(int i)
 {
-
 	if (i > 0)
-        return (i);
-        else if (i < 0)
-        return (-i);
-        else
-        return (0);
+	return (i);
+	else if(i < 0)
+	return (-i);
+	else
+	return (0);
 }
