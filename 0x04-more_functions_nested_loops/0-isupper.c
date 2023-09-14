da92c392814849f61714e86e46cfea1d607bbbf5
Minou90 <amina.green.smart@gmail.com>
2023-09-14 15:07:38 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,12 +2,12 @@
 
 /**
  * _isupper - uppercase letters
- * @c: character to check
+ * @c: char to check
  *
  * Return: 0 or 1
  */
-int _isupper(int c)
 
+int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 		return (1);
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,17 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c)
-int _isupper(int c)
-int _isdigit(int c)
-int _mul(int a, int b);
-void _print_numbers(void);
-void _print_most_numbers(void);
-void _more_numbers(void);
-void _print_line(int n);
-void _print_diagonal(int n);
-void _print_square(int size);
-void _print_triangle(int size);
-void _print_number(int n);
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
 
 #endif
